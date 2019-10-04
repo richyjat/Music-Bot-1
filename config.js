eb30731ea7e4b89827f2aa5e52d3a05014445fbc
richyjat <49512329+richyjat@users.noreply.github.com>
2019-10-04 15:28:34 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-exports.TOKEN = '';
+exports.TOKEN = 'NjI5NTE3MTE5MjIyMTg2MDA2.XZcV8A.syX_prxz3-L0e9yQqz8Xr-kiphI';
 
-exports.PREFIX = 'm!';
+exports.PREFIX = '++';
 
-exports.GOOGLE_API_KEY = '';
+exports.GOOGLE_API_KEY = 'AIzaSyCrLt19NqEFVEC9fTZ6c1F4P_Te-6fqSK8';
